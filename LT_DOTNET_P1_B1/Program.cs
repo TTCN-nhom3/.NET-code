2f595c8de3a25b280be213cbcb311e77adef2d13
KienVu622 <kienvuhd02@gmail.com>
2023-03-18 16:32:25 +0700

    C# bai 1

---
+++ LT_DOTNET_P1_B1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LT_DOTNET_P1_B1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
